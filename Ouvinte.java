8d28d86792065104a173e23beed670280facaa4c
Bruno Macedo <macedosauro@id.uff.br>
2020-10-20 17:25:08 -0300

    Ouvinte finalizado se pá

--- Ouvinte.java
+++ Ouvinte.java
@@ -10,9 +10,24 @@ import java.util.Scanner;
 public class Ouvinte implements IOuvinte{
     public void notificar( String dados ) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n\n\n//----------------// N O T I F I C A Ç Ã O //------------------//\n");
         System.out.print( dados );
+        System.out.println("\n\n//----------------// N O T I F I C A Ç Ã O //------------------//\n\n\n");
         sc.next();
-   }
+    }
+
+    private void registrarTopico( INotificador servidor, String id ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Qual o nome do tópico que deseja se registrar? ");
+        String topico = sc.nextLine();
+
+        try {
+        servidor.registrar( topico, id );
+        } catch(Exception e) {
+            System.err.println("Exception:");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args){
         if (System.getSecurityManager() == null) {
@@ -22,15 +37,17 @@ public class Ouvinte implements IOuvinte{
             Scanner sc = new Scanner(System.in);
 
             //Gerando o ID aleatório.
-            Random gerador =new Random();
+            Random gerador = new Random();
             String ID = Integer.toString(gerador.nextInt());
 
             //Criando o o bjeto remoto e exportando e registrando no rmiregistry
-            IOuvinte ouvinte = new Ouvinte();
+            Ouvinte ouvinte = new Ouvinte();
             IOuvinte stub = (IOuvinte) UnicastRemoteObject.exportObject(ouvinte, 0);
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("nome_aleatorio", stub);
-            /*
+
+            INotificador servidor = (INotificador) registry.lookup("Notificador");
+
             //Menuzinho
             int opcao = -1;
             while(opcao != 0){
@@ -42,10 +59,9 @@ public class Ouvinte implements IOuvinte{
                 sc.nextLine();
 
                 if (opcao == 1){
-                    //this.registrarTopico();//Não completei, parei aqui
+                    ouvinte.registrarTopico(servidor, ID);//Não completei, parei aqui
                 }
             }
-            */
         } catch (Exception e) {
             System.err.println("Exception:");
             e.printStackTrace();
